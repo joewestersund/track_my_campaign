0375d452706c911e8e4fcc9bc6bef5799ca6cea0
Joe Westersund <joe_westersund@hotmail.com>
2014-02-07 21:05:31 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrackMyCampaign::Application.config.secret_key_base = '8854e25e6e1ef37c7fac15b185b42d835a3d3212ed425cfae94184f9e773701252bf6a926ce3899d6f50b60a17d5d69947a78aad797dc89b517c00daeb73f1e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrackMyCampaign::Application.config.session_store :cookie_store, key: '_track_my_campaign_session'
