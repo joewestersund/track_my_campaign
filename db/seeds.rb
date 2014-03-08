f076d25fc8cbd4263ddca499e35381a3e0aee757
Joe Westersund <joe_westersund@hotmail.com>
2014-03-08 15:10:30 -0800

    add rake task to upload CCPHA contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,6 @@ UserPermission.destroy_all
 DatabaseInstance.destroy_all
 User.delete_all
 
-default_password = 'testing'
+DEFAULT_PASSWORD = 'testing'
 admin_user = User.create({first_name: 'Joe', last_name: 'Westersund', email: 'joe_westersund@hotmail.com', password: default_password, password_confirmation: default_password, admin: true})
 
-user_iphi = User.create({first_name: 'Marisa', last_name: 'Jones', email: 'mjones@institutephi.org', password: default_password, password_confirmation: default_password })
-user_ccpha = User.create({first_name: 'Kanat', last_name: 'Tibet', email: 'kt@publichealthadvocacy.org', password: default_password, password_confirmation: default_password })
-
-dbi_iphi = Heal::DatabaseInstance.create({instance_name: 'IPHI-test'})
-dbi_ccpha = Heal::DatabaseInstance.create({instance_name: 'CCPHA-test'})
-
-UserPermission.create({database_instance: dbi_iphi, user: user_iphi})
-UserPermission.create({database_instance: dbi_ccpha, user: user_ccpha})
